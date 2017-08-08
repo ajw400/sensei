fbf56c338b297c8adee46d88e49ec20975f9cde7
Pim Hulskorte <pimhulskorte@gmail.com>
2017-08-08 12:34:50 +0200

    lessoncontroller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,4 @@
 Rails.application.routes.draw do
-  get 'appointments/confirm'
-
-  get 'appointments/deny'
-
-  get 'appointments/cancel'
-
-  get 'appointments/create'
-
-  get 'profiles/edit'
-
-  get 'profiles/show'
-
-  get 'profiles/update'
-
-  get 'lessons/index'
-
-  get 'lessons/show'
-
-  get 'lessons/new'
-
-  get 'lessons/update'
-
-  get 'lessons/destroy'
-
-  get 'lessons/create'
-
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
