563a254d49f995bdf4e9d83febba88885b504378
Pim Hulskorte <pimhulskorte@gmail.com>
2017-08-08 16:24:07 +0200

    um

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :appointments
   has_many :lessons
   has_many :subcategories, through: :lessons
-  has_many :appointments, through: :lessons
+  has_many :received_appointments, source: :appointments, through: :lessons
 end
