3d0e17c103c636e6b4f5258a4c56a2a95afa6203
Andrew Wright <prestoassai@hotmail.com>
2017-08-09 22:45:17 +0200

    fixed appts

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,17 +1,23 @@
 class AppointmentsController < ApplicationController
+  before_action :set_appointment, only: [:confirm, :deny, :cancel]
+
   def confirm
+    @lesson.update!(status: "confirmed")
   end
 
   def deny
+    @lesson.update!(status: "denied")
   end
 
   def cancel
+    @lesson.update!(status: "cancelled")
   end
 
   def create
     appt = Appointment.new(appointment_params)
     appt.user = current_user
     appt.lesson = Lesson.find(params[:lesson_id])
+    appt.status = "unconfirmed"
     # appt.total_price = params[:length] * appt.lesson.hour_price
     appt.save!
     redirect_to profile_path
@@ -21,5 +27,9 @@ class AppointmentsController < ApplicationController
 
   def appointment_params
     params.require(:appointment).permit(:date_time, :length)
+  end
+
+  def set_appointment
+    @lesson = Appointment.find(params[:id])
   end
 end
